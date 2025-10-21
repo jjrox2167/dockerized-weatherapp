a77a0f6a2ce9d0cbc41752290541f91107485634
jjrox2167 <104599883+jjrox2167@users.noreply.github.com>
2025-10-20 23:00:03 -0500

    Created dockerfile and updated README.MD

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'node .next/standalone/server.js',
 };
 
 module.exports = nextConfig;
